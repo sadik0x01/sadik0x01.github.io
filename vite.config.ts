837e1e7898e89c9aa2c03c1a3edbd998d53d1791
SaDiK MaHmuD <sadik0x01@gmail.com>
2025-10-15 00:02:17 +0600

    Save local changes before pulling

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/sadik0x01",
   plugins: [
     react(),
     runtimeErrorOverlay(),
